51b4e3f32a24f8bcaae1fda80b762a4d85003a10
eppleton <info@eppleton.de>
2016-12-21 06:18:35 +0100

    Once per minute is enough

--- .travis.sh
+++ .travis.sh
@@ -6,7 +6,7 @@ mvn test&
 PID=$!
 
 while true; do
-    sleep 10
+    sleep 60
     if kill -0 $PID >/dev/null 2>&1; then
         echo Process $PID is still running...
         SURE=`jps | grep surefire | cut -f 1 -d " "`
