682e5785178427c4a75aa7e0a27c361e2730d391
eppleton <info@eppleton.de>
2016-12-21 06:02:50 +0100

    Dump what is the status of test execution

---
+++ .travis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mvn install -DskipTests
+
+mvn -f generic test&
+PID=$!
+
+while true; do
+    sleep 10
+    if kill -0 $PID >/dev/null 2>&1; then
+        echo Process $PID is still running...
+        SURE=`jps | grep surefire | cut -f 1 -d " "`
+        echo Surefire PID: $SURE
+        if [ -n "$SURE" ]; then
+            jstack $SURE
+        fi
+    else
+        echo Finished OK...
+        exit 0
+    fi
+done
