2796e832bd365769ab8114292ed5b1de900dda66
eppleton <info@eppleton.de>
2016-12-21 07:42:48 +0100

    Execute only strings and generic tests as they are able to succeed

--- .travis.sh
+++ .travis.sh
@@ -2,21 +2,33 @@
 
 mvn install -DskipTests
 
-mvn test&
-PID=$!
 
-while true; do
-    sleep 480
-    if kill -0 $PID >/dev/null 2>&1; then
-        echo Process $PID is still running...
-        SURE=`jps | grep surefire | cut -f 1 -d " "`
-        echo Surefire PID: $SURE
-        if [ -n "$SURE" ]; then
-            jstack $SURE
+function waitFor {
+    PID=$1
+    COUNTDOWN=48
+    while true; do
+        sleep 10
+        if kill -0 $PID >/dev/null 2>&1; then
+            if [ $COUNTDOWN > 0 ]; then
+                COUNTDOWN=`expr $COUNTDOWN - 1`
+                continue
+            fi
+            echo Process $PID is still running...
+            SURE=`jps | grep surefire | cut -f 1 -d " "`
+            echo Surefire PID: $SURE
+            if [ -n "$SURE" ]; then
+                jstack $SURE
+            fi
+            jps -v
+        else
+            echo Finished OK...
+            return
         fi
-        jps -v
-    else
-        echo Finished OK...
-        exit 0
-    fi
-done
+    done
+}
+
+mvn -f strings test&
+waitFor $!
+
+mvn -f generic test&
+waitFor $!
