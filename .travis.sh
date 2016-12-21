2ea9898afa73422b754f943cec912e9cfdf8d369
eppleton <info@eppleton.de>
2016-12-21 07:07:45 +0100

    Print out the dump once per eight minutes

--- .travis.sh
+++ .travis.sh
@@ -6,7 +6,7 @@ mvn test&
 PID=$!
 
 while true; do
-    sleep 60
+    sleep 480
     if kill -0 $PID >/dev/null 2>&1; then
         echo Process $PID is still running...
         SURE=`jps | grep surefire | cut -f 1 -d " "`
