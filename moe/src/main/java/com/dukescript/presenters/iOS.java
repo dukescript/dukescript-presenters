8cb7962dd4ed3edb9bdbeec00ab41e37d1488af3
eppleton <info@eppleton.de>
2017-06-26 05:43:50 +0200

    Moving skeleton of MoeApplication to separate package

--- moe/src/main/java/com/dukescript/presenters/iOS.java
+++ moe/src/main/java/com/dukescript/presenters/iOS.java
@@ -24,43 +24,27 @@ package com.dukescript.presenters;
  */
 import java.io.Closeable;
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.moe.natj.general.ann.RegisterOnStartup;
-import org.moe.natj.general.ann.Runtime;
-import org.moe.natj.objc.ObjCRuntime;
 import org.netbeans.html.boot.spi.Fn;
 import org.openide.util.lookup.ServiceProvider;
 
-import apple.NSObject;
-import apple.coregraphics.struct.CGRect;
 import apple.foundation.NSBundle;
-import apple.foundation.NSDictionary;
 import apple.foundation.NSError;
 import apple.foundation.NSOperation;
 import apple.foundation.NSOperationQueue;
 import apple.foundation.NSURL;
 import apple.foundation.NSURLRequest;
 import apple.uikit.UIApplication;
-import apple.uikit.protocol.UIApplicationDelegate;
-import apple.uikit.UIColor;
-import apple.uikit.UIScreen;
-import apple.uikit.enums.UIViewAutoresizing;
-import apple.uikit.UIViewController;
 import apple.uikit.UIWebView;
 import apple.uikit.protocol.UIWebViewDelegate;
-import apple.uikit.UIWindow;
-import apple.uikit.c.UIKit;
+import com.dukescript.presenters.moe.MoeApplication;
 import java.io.Flushable;
-import org.moe.natj.general.NatJ;
 import org.moe.natj.general.Pointer;
-import org.moe.natj.objc.ann.ObjCClassName;
 import org.moe.natj.objc.ann.Selector;
 
 /** Ultimate <a href="http://dukescript.com">DukeScript</a>
@@ -202,103 +186,13 @@ public final class iOS extends Generic
     public void displayPage(URL page, Runnable onPageLoad) {
 //        NSAutoreleasePool pool = new NSAutoreleasePool();
         try {
-            App.mainView(page.toExternalForm(), new WebViewDelegate(onPageLoad));
+            MoeApplication.mainView(page.toExternalForm(), new WebViewDelegate(onPageLoad));
         } catch (RuntimeException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new IllegalStateException(ex);
         }
 //        pool.close();
-    }
-
-    @Runtime(ObjCRuntime.class)
-    @ObjCClassName("iOSApp")
-    @RegisterOnStartup
-    static final class App extends NSObject implements UIApplicationDelegate {
-
-        private static UIWindow window;
-        private static UIWebView webView;
-        private static UIWebViewDelegate delegate;
-        private static String page;
-        private static CountDownLatch waitFor;
-
-        protected App(Pointer p) {
-            super(p);
-        }
-
-        @Override
-        public boolean applicationDidFinishLaunchingWithOptions(UIApplication application, NSDictionary<?, ?> launchOptions) {
-            application.setStatusBarHidden(false);
-
-            final CGRect bounds = UIScreen.mainScreen().bounds();
-            webView = UIWebView.alloc().initWithFrame(bounds);
-            webView.setDelegate(delegate);
-            webView.setAutoresizingMask(
-                    UIViewAutoresizing.FlexibleBottomMargin |
-                    UIViewAutoresizing.FlexibleHeight |
-                    UIViewAutoresizing.FlexibleLeftMargin |
-                    UIViewAutoresizing.FlexibleRightMargin |
-                    UIViewAutoresizing.FlexibleTopMargin |
-                    UIViewAutoresizing.FlexibleWidth
-            );
-            CGRect whole = UIScreen.mainScreen().bounds();
-            window = UIWindow.alloc().initWithFrame(whole);
-            window.setRootViewController(MainController.alloc());
-            window.rootViewController().setView(webView);
-            window.setBackgroundColor(UIColor.whiteColor());
-            window.addSubview(webView);
-            window.makeKeyAndVisible();
-
-            NSURLRequest req = NSURLRequest.requestWithURL(NSURL.URLWithString(page));
-            webView.loadRequest(req);
-
-            waitFor.countDown();
-            return true;
-        }
-
-        static UIWebView mainView(String p, UIWebViewDelegate d) throws IOException {
-            page = p;
-            waitFor = new CountDownLatch(1);
-            delegate = d;
-            UIKit.UIApplicationMain(0, null, null, "iOSApp");
-            try {
-                waitFor.await();
-            } catch (InterruptedException ex) {
-                throw new InterruptedIOException();
-            }
-            return webView;
-        }
-
-        @Runtime(ObjCRuntime.class)
-        @RegisterOnStartup
-        @ObjCClassName("MainController")
-        final static class MainController extends UIViewController {
-            static {
-                NatJ.register();
-            }
-
-            protected MainController(Pointer pntr) {
-                super(pntr);
-            }
-
-            @Selector("alloc")
-            public static native MainController alloc();
-
-            @Override
-            public boolean prefersStatusBarHidden() {
-                return true;
-            }
-
-            @Override
-            public boolean shouldAutorotate() {
-                return true;
-            }
-
-            @Override
-            public boolean shouldAutomaticallyForwardRotationMethods() {
-                return false;
-            }
-        }
     }
 
     private final class WebViewDelegate implements UIWebViewDelegate {
